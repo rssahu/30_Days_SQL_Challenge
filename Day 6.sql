d840f0b7cce5ae554d684387df016c3b427d6b20
rssahu <106058808+rssahu@users.noreply.github.com>
2024-09-22 16:52:18 +0530

    Update Day 6.sql

--- Day 6.sql
+++ Day 6.sql
@@ -53,8 +53,7 @@ FROM viewership;
 -- Question link https://datalemur.com/questions/laptop-mobile-viewership
 
 /*	
-Follow me in LinkedIn :: https://www.linkedin.com/in/najirr/
-Follow me in insta :: https://www.instagram.com/zero_analyst/
-Subscribe to our youtube channel :: https://www.youtube.com/@zero_analyst
-*/
+Follow me on LinkedIn:: https://www.linkedin.com/in/rajeshmsahu12/
+Subscribe to our YouTube channel:: https://www.youtube.com/@sonamsahukitchen1471
+
 
